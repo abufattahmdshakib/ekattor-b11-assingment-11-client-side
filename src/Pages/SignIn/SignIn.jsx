3dd33ed5acfb47c75eaad6de978bacf04d5d4700
abufattahmdshakib <abufattahmdshakib21@gmail.com>
2025-06-19 16:55:48 +0600

    make a singin page

---
+++ src/Pages/SignIn/SignIn.jsx
@@ -0,0 +1,140 @@
+import React, { use, useState } from "react";
+import { Link, useLocation, useNavigate } from "react-router";
+import { AuthContext } from "../../Context/AuthContext";
+import Swal from "sweetalert2";
+
+const SignIn = () => {
+  const { signInUser, signInWithGoogle } = use(AuthContext);
+  const [erroMesseage, setErrorMessage] = useState(false);
+  const location = useLocation();
+  const navigate = useNavigate();
+  const handleSignIn = (e) => {
+    e.preventDefault();
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    console.log({ email, password });
+
+    setErrorMessage("");
+
+    signInUser(email, password)
+      .then((result) => {
+        console.log(result);
+        navigate(`${location.state ? location.state : "/"}`);
+      })
+      .catch((error) => {
+        console.log(error);
+        setErrorMessage(error.message);
+      });
+  };
+
+  const handleSignInWithGoogle = () => {
+    signInWithGoogle()
+      .then((result) => {
+        console.log(result);
+        // fetch("https://uddog-server.vercel.app/users", {
+        //   method: "POST",
+        //   headers: {
+        //     "Content-Type": "application/json",
+        //   },
+        //   body: JSON.stringify(provider),
+        // })
+        //   .then((response) => response.json())
+        //   .then((data) => {
+        //     console.log(data);
+        //     if (data.insertedId) {
+        //       Swal.fire({
+        //         title: "Account Created Successfully!",
+        //         icon: "success",
+        //         draggable: true,
+        //       });
+        //     }
+        //   });
+        navigate(`${location.state ? location.state : "/"}`);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  return (
+    <div className="hero bg-base-200 min-h-screen">
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+          <div className="card-body">
+            <div className="text-center lg:text-left">
+              <h1 className="text-5xl font-bold">Login now!</h1>
+            </div>
+            <form onSubmit={handleSignIn} className="fieldset">
+              <label className="label">Email</label>
+              <input
+                type="email"
+                name="email"
+                className="input"
+                placeholder="Email"
+              />
+              <label className="label">Password</label>
+              <input
+                type="password"
+                name="password"
+                className="input"
+                placeholder="Password"
+              />
+              <div>
+                <a className="link link-hover">Forgot password?</a>
+                {erroMesseage && <p className="text-red-400">{erroMesseage}</p>}
+              </div>
+              <button className="relative inline-block px-4 py-2 font-medium group">
+            <span className="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-[#129ee7] group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
+            <span className="absolute inset-0 w-full h-full bg-white border-2 border-[#129ee7] group-hover:bg-[#129ee7]"></span>
+            <span className="relative text-black group-hover:text-white">
+              Sign In
+            </span>
+          </button>
+              {/* Google */}
+              <button
+                type="button"
+                onClick={handleSignInWithGoogle}
+                className="btn bg-white text-black border-[#e5e5e5]"
+              >
+                <svg
+                  aria-label="Google logo"
+                  width="16"
+                  height="16"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 512 512"
+                >
+                  <g>
+                    <path d="m0 0H512V512H0" fill="#fff"></path>
+                    <path
+                      fill="#34a853"
+                      d="M153 292c30 82 118 95 171 60h62v48A192 192 0 0190 341"
+                    ></path>
+                    <path
+                      fill="#4285f4"
+                      d="m386 400a140 175 0 0053-179H260v74h102q-7 37-38 57"
+                    ></path>
+                    <path
+                      fill="#fbbc02"
+                      d="m90 341a208 200 0 010-171l63 49q-12 37 0 73"
+                    ></path>
+                    <path
+                      fill="#ea4335"
+                      d="m153 219c22-69 116-109 179-50l55-54c-78-75-230-72-297 55"
+                    ></path>
+                  </g>
+                </svg>
+                Login with Google
+              </button>
+            </form>
+            <button>
+              Do You have an Account?
+              <Link to="/auth/signUp">Register</Link>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SignIn;

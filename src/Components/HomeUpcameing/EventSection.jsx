7d0936a3d6c62165ed7349f2f1a88c5926db4ef7
abufattahmdshakib <abufattahmdshakib21@gmail.com>
2025-08-11 01:37:28 +0600

    ok

--- src/Components/HomeUpcameing/EventSection.jsx
+++ src/Components/HomeUpcameing/EventSection.jsx
@@ -70,7 +70,7 @@ export default function EventsSection  ({
 
             {selectedEvent && (
                 <div
-                    className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center p-4 z-50"
+                    className="fixed text-black inset-0 bg-black bg-opacity-50 flex justify-center items-center p-4 z-50"
                     onClick={() => setSelectedEvent(null)}
                 >
                     <div
